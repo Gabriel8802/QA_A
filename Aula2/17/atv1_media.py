b9f60a70153c757a6d0b771fb235cd2daf937049
Gabriel <gabriel8802@gmail.com>
2024-06-17 20:28:45 -0300

    atividade 1 aula2 média

---
+++ Aula2/17/atv1_media.py
@@ -0,0 +1,6 @@
+n1 = float (input('Qual foi sua nota de português?'))
+n2 = float (input('Qual sua nota de matemática?'))
+n3 = float (input('Qual sua nota de ciências?'))
+n4 = float (input('Qual sua nota de biologia?'))
+media = float(n1 + n2 + n3 + n4) / 4
+print('sua media e',media)
