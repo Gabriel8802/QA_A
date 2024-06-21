cd82e3b53a980d5c67987ceff782b0a300c8abc5
Gabriel <gabriel8802@gmail.com>
2024-06-20 22:18:36 -0300

    estudando

---
+++ program_type.py
@@ -0,0 +1,12 @@
+hobby=input('Qual é o seu hobby ?')
+horas=int(input('Que horas são agora ?'))
+altura=float(input('Qual sua altura?'))
+
+print('O seu hobby é {} e agora são {}' .format (hobby,horas))
+
+print(type(hobby,horas,altura))
+
+#como colocar : para horas?
+#como colocar , ao invés de . na altura?
+#Porque o comando type só mostra uma variavel de um grupo com diferente?.
+
