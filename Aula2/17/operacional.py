e75bcd093c1a256edac6498b24259356d284d571
Gabriel <gabriel8802@gmail.com>
2024-07-06 12:30:31 -0300

    operacional

---
+++ Aula2/17/operacional.py
@@ -0,0 +1,7 @@
+valor=float(input('digite o valor:'))
+print(f"o dobro é =",valor*2)
+print(f"o triplo é =",valor*3)
+print(f"o valor ao quadrado é =",valor**2)
+print(f"o valor da raiz quadrada é =",valor**0,5)
+print(f"o valor da raiz cubica é =",valor**(1/3))
+
