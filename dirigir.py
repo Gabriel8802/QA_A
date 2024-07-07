7fd17ba05edf750d24a0791809ecf869399b36af
Gabriel <gabriel8802@gmail.com>
2024-07-07 12:24:26 -0300

    update_elif

--- dirigir.py
+++ dirigir.py
@@ -1,5 +1,7 @@
 idade = int(input('Qual a sua idade?'))
 if idade > 18:
     print("individuo possui idade mínima pra dirigir")
+elif idade == 17 or idade == 18:
+    print("individuo ainda não está apto para dirigir")
 else:
-    print("proibido dirigir")
+    print("individuo não possui idade mínima pra dirigir")
