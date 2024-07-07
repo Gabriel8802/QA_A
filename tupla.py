3b016c048163b49e82c1ab979e6b75649a3266e8
Gabriel <gabriel8802@gmail.com>
2024-07-07 11:39:52 -0300

    atividade_tupla

---
+++ tupla.py
@@ -0,0 +1,18 @@
+#crie uma tupla com 5 iteis
+tupla = ['Futebol','basquete','volei','natação','golf']
+#converta a tupla para lista
+lista = list(tupla)
+#adicionar dois dados extra na lista
+lista.append('boxe')
+lista.append('tenis')
+#remova o ultimo dado da lista
+lista.remove('tenis')
+print(lista)
+
+dicionario={
+"nome": "Gabriel",
+"idade": "29",
+"profissão": "Jardineiro"
+}
+
+print(dicionario["nome"])
