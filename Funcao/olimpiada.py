d76f91582fa3f27ab761eba655a3ac8ce1d8f133
Gabriel <gabriel8802@gmail.com>
2024-07-14 10:29:52 -0300

    você é do time...

---
+++ Funcao/olimpiada.py
@@ -0,0 +1,14 @@
+def alunos(numero):
+    if numero % 2 == 0:
+        print('você é do time azul')
+    else:
+        print('você é do time amarelo')
+
+while True:
+    n1=int(input('digite um número de 1 até 100:'))
+    if n1 <= 100:
+        break
+
+resultado=alunos(n1)
+print(resultado)
+
