3a7201afec28c237013b721c66df7464d9d73bc0
Gabriel <gabriel8802@gmail.com>
2024-07-14 09:39:26 -0300

    soma dois numeros

---
+++ Funcao/soma_dois_numeros.py
@@ -0,0 +1,8 @@
+def soma(n1,n2):
+    somando=n1+n2
+    return somando
+v1=float(input('Digite o numero:'))
+v2=float(input('Digite o segundo número:'))
+
+resultado=soma(v1,v2)
+print('O valor da soma é:',resultado)
