ab85a5d7a5c5504bf48282471476b2d3fe60df8d
Gabriel <gabriel8802@gmail.com>
2024-07-21 07:35:49 -0300

    pandas

--- massa_de_dados/Filtre_dados.py
+++ massa_de_dados/Filtre_dados.py
@@ -1,1 +1,19 @@
-dados = dados_ficticios.csv.get(idade)
+import pandas as pd
+
+df = pd.read_csv('massa_de_dados/dados_ficticios.csv')
+#print(df)
+
+#com idade maior que 40 anos
+idade40 = df[df['idade'] > 40]
+print('Pessoas acima de 40 anos')
+print(idade40)
+
+#com renda maior de 5 mil
+renda5000 = df[df['renda'] > 5000]
+print('\nPessoas com renda acima de R$5.000,00')
+print(renda5000)
+
+#com renda maior de 15 mil
+renda15000 = df[df['renda'] > 15000]
+print('\nPessoas com renda acima de R$15.000,00')
+print(renda15000)
